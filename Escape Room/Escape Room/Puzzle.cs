e25c4c2198bb8e09aefabb26a7908967f3768fe5
BoboParaskevov <bparaskevov007@gmail.com>
2025-06-18 19:36:29 +0300

    Update Puzzle.cs

--- Escape Room/Escape Room/Puzzle.cs
+++ Escape Room/Escape Room/Puzzle.cs
@@ -6,27 +6,26 @@ using System.Threading.Tasks;
 
 namespace Escape_Room
 {
-    internal class Puzzle
-    {
-        private string key;
-        private string description;
-        private string answer;
-        private Item reward;
-        private bool solved;
+     internal class Puzzle
+ {
+     private string key;
+     private string description;
+     private string answer;
+     private Item reward;
+     private bool solved;
 
-        public string Key { get => key; set => key = value; }
-        public string Description { get => description; set => description = value; }
-        public string Answer { get => answer; set => answer = value; }
-        public bool Solved { get => solved; set => solved = value; }
-        internal Item Reward { get => reward; set => reward = value; }
+     public string Key { get => key; set => key = value; }
+     public string Description { get => description; set => description = value; }
+     public string Answer { get => answer; set => answer = value; }
+     public bool Solved { get => solved; set => solved = value; }
+     internal Item Reward { get => reward; set => reward = value; }
 
-        public Puzzle(string key, string description, string answer, Item reward)
-        {
-            this.key = key;
-            this.description = description;
-            this.answer = answer;
-            this.reward = reward;
-            solved = false;
-        }
-    }
-}
+     public Puzzle(string key, string description, string answer, Item reward)
+     {
+         this.key = key;
+         this.description = description;
+         this.answer = answer;
+         this.reward = reward;
+         solved = false;
+     }
+ }
