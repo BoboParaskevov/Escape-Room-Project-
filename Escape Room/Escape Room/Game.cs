d3a941bc60c6090c75cf2cda3282718aa68f8c36
BoboParaskevov <bparaskevov007@gmail.com>
2025-06-18 19:32:44 +0300

    Update Game.cs

--- Escape Room/Escape Room/Game.cs
+++ Escape Room/Escape Room/Game.cs
@@ -6,191 +6,190 @@ using System.Threading.Tasks;
 
 namespace Escape_Room
 {
-    internal class Game
+   internal class Game
+{
+    private Player player;
+    private List<Room> rooms;
+    private int location;
+    private int roomsSolved;
+    private List<Item> mergedItems = new List<Item>();
+
+    internal Player Player { get => player; set => player = value; }
+    internal List<Room> Rooms { get => rooms; set => rooms = value; }
+    public int Location { get => location; set => location = value; }
+    internal List<Item> MergedItems { get => mergedItems; set => mergedItems = value; }
+
+    public Game(Player player, List<Room> rooms)
     {
-        private Player player;
-        private List<Room> rooms;
-        private int location;
-        private int roomsSolved;
-        private List<Item> mergedItems;
+        this.player = player;
+        this.rooms = rooms;
+        location = 0;
+        roomsSolved = 0;
 
-        internal Player Player { get => player; set => player = value; }
-        internal List<Room> Rooms { get => rooms; set => rooms = value; }
-        public int Location { get => location; set => location = value; }
-        internal List<Item> MergedItems { get => mergedItems; set => mergedItems = value; }
-
-        public Game(Player player, List<Room> rooms)
+        if (File.Exists("merged_items.txt"))
         {
-            this.player = player;
-            this.rooms = rooms;
-            location = 0;
-            roomsSolved = 0;
-            StreamReader sr = new StreamReader("merged_items.txt");
-            while (!sr.EndOfStream)
+            using (StreamReader sr = new StreamReader("merged_items.txt"))
             {
-                string line = sr.ReadLine();
-                string name = line.Substring(0, line.IndexOf(" ") - 1);
-                string description = line.Substring(line.IndexOf("-") + 2);
-                Item item = new Item (name, description);
-                mergedItems.Add(item);
-            }
-        }
-
-        public void Start()
-        {
-            EnterRoom(location);
-            while (roomsSolved < rooms.Count)
-            {
-                Console.WriteLine("You've another room");
-                EnterRoom(location);
-            }
-        }
-
-        public void EnterRoom(int number)
-        {
-            Console.WriteLine(rooms[number].Description);
-            bool checkInput = false;
-            while (!rooms[number].Solved)
-            {
-                string input = Console.ReadLine();
-                foreach (Puzzle puzzle in rooms[number].Puzzles)
+                while (!sr.EndOfStream)
                 {
-                    if (input == puzzle.Key)
-                    {
-                        checkInput = true;
-                        Console.WriteLine(puzzle.Description);
-                        while (!puzzle.Solved)
-                        {
-                            string guess = Console.ReadLine();
-                            if (guess == puzzle.Answer)
-                            {
-                                puzzle.Solved = true;
-                                player.Add(puzzle.Reward);
-                                rooms[number].PuzzlesSolved++;
-                                Console.WriteLine($"You got a {puzzle.Reward.Name} - {puzzle.Reward.Description}");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Wrong answer");
-                            }
-                        }
-                    }
+                    string line = sr.ReadLine();
+                    string name = line.Substring(0, line.IndexOf(" "));
+                    string description = line.Substring(line.IndexOf("-") + 2);
+                    Item item = new Item(name, description);
+                    mergedItems.Add(item);
                 }
-                if (input == rooms[number].Key.Name)
+            }
+        }
+    }
+
+    public void Start()
+    {
+        EnterRoom(location);
+        while (roomsSolved < rooms.Count)
+        {
+            Console.WriteLine("\nYou have another room to explore.");
+            EnterRoom(location);
+        }
+        Console.WriteLine("Congratulations! You've completed all rooms.");
+    }
+
+    public void EnterRoom(int number)
+    {
+        Room currentRoom = rooms[number];
+        Console.WriteLine("\n" + currentRoom.Description);
+
+        bool roomCompleted = false;
+        while (!roomCompleted)
+        {
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1. Type puzzle key(wardrobe, chest, equation)");
+            Console.WriteLine("2. Inventory");
+            Console.WriteLine("3. Use Item(Bobby Pin, Equation, Key)");
+            string input = Console.ReadLine().Trim().ToLower();
+
+            if (input == "inventory")
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine(player.CheckInventory());
+                continue;
+            }
+
+            if (input.StartsWith("use "))
+            {
+                string itemToUse = input.Substring(4).Trim();
+                if (itemToUse == "bobby pin")
                 {
-                    checkInput = true;
-                    bool posession = false;
-                    foreach (Item item in player.Inventory)
+                    Puzzle chestPuzzle = currentRoom.Puzzles.Find(p => p.Key == "chest");
+                    if (chestPuzzle != null && !chestPuzzle.Solved)
                     {
-                        if (item.Name == input)
+                        bool hasBobbyPin = player.Inventory.Exists(i => i.Name.ToLower().Contains("bobby pin"));
+                        if (hasBobbyPin)
                         {
-                            posession = true;
+                            chestPuzzle.Solved = true;
+                            player.Add(chestPuzzle.Reward); 
+                            Console.WriteLine("\nYou unlock the chest with the bobby pin. Inside, you find a math equation.");
                         }
-                    }
-                    if (posession == false)
-                    {
-                        Console.WriteLine("You do not have this item");
-                        
-                    }
-                    else if (rooms[number].PuzzlesSolved < rooms[number].Puzzles.Count)
-                    {
-                        Console.WriteLine("There are still puzzles to solve in this room");
+                        else
+                        {
+                            Console.WriteLine("\nYou don't have a bobby pin to pick the lock.");
+                        }
                     }
                     else
                     {
-                        rooms[number].Solved = true;
-                    }
-
-                }
-                else if (checkInput == false)
-                {
-                    Console.WriteLine("This input does nothing");
-                }
-            }
-            Console.WriteLine("Exiting room...");
-            location++;
-            Thread.Sleep(3000);
-        }
-
-        public void Merge (string firstItem,  string secondItem)
-        {
-            StreamReader sr = new StreamReader("merging.txt");
-            while (!sr.EndOfStream)
-            {
-                string line = sr.ReadLine();
-                int number = 0;
-                foreach (char c in line)
-                {
-                    if (c == '+')
-                    {
-                        number++;
-                    }
-                }
-                if (number == 1)
-                {
-                    if (line.Substring(0, line.IndexOf(" ") - 1) == firstItem)
-                    {
-                        if (line.Substring(line.IndexOf("+") + 2, line.IndexOf("=") - 2) == secondItem)
-                        {
-                            foreach (Item item in mergedItems)
-                            {
-                                if (item.Name == line.Substring(line.IndexOf("=") + 2))
-                                {
-                                    player.Add(item);
-                                }
-                            }
-                        }
+                        Console.WriteLine("\nThe chest is already unlocked or doesn't exist here.");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("You can't merge those items");
-                }     
-            }
-        }
-
-        public void Merge(string firstItem, string secondItem, string thirdItem)
-        {
-            StreamReader sr = new StreamReader("merging.txt");
-            while (!sr.EndOfStream)
-            {
-                string line = sr.ReadLine();
-                int number = 0;
-                foreach (char c in line)
-                {
-                    if (c == '+')
-                    {
-                        number++;
-                    }
+                    Console.WriteLine($"\nYou can't use {itemToUse} here.");
                 }
-                if (number == 1)
+                continue;
+            }
+
+            Puzzle puzzle = currentRoom.Puzzles.Find(p => p.Key.ToLower() == input);
+            if (puzzle != null && !puzzle.Solved)
+            {
+                Console.WriteLine("\nPuzzle: " + puzzle.Description);
+                bool solved = false;
+
+                while (!solved)
                 {
-                    if (line.Substring(0, line.IndexOf(" ") - 1) == firstItem)
+                    Console.Write("Enter your answer: ");
+                    string answer = Console.ReadLine().Trim();
+
+                    if (answer.Equals(puzzle.Answer, StringComparison.OrdinalIgnoreCase))
                     {
-                        if (line.Substring(line.IndexOf("+") + 2, line.IndexOf("=") - 2) == secondItem)
+                        puzzle.Solved = true;
+                        player.Add(puzzle.Reward);
+                        Console.WriteLine($"\nCorrect! You received: {puzzle.Reward.Name} - {puzzle.Reward.Description}");
+                        solved = true;
+
+                        if (puzzle.Key == "equation")
                         {
-                            string shortLine = line.Substring(line.IndexOf("+") + 1);
-                            if (shortLine.Substring(shortLine.IndexOf("+") + 2, shortLine.IndexOf("=") - 2) == thirdItem)
-                            foreach (Item item in mergedItems)
-                            {
-                                if (item.Name == line.Substring(line.IndexOf("=") + 2))
-                                {
-                                    player.Add(item);
-                                }
-                            }
+                            Console.WriteLine("\nYou solved the math equation! You can now open the glass box.");
                         }
                     }
+                    else
+                    {
+                        Console.WriteLine("\nThat's not correct. Try again.");
+                    }
+                }
+            }
+            else if (input == currentRoom.Key.Name.ToLower())
+            {
+                bool hasKey = player.Inventory.Exists(i => i.Name.ToLower() == currentRoom.Key.Name.ToLower());
+                if (!hasKey)
+                {
+                    Console.WriteLine("\nThe door is locked. You need the key.");
+                }
+                else if (currentRoom.Puzzles.Exists(p => !p.Solved))
+                {
+                    Console.WriteLine("\nYou must solve all puzzles in this room before exiting.");
                 }
                 else
                 {
-                    Console.WriteLine("You can't merge those items");
+                    Console.WriteLine("\nYou unlock the door and exit the room.");
+                    currentRoom.Solved = true;
+                    location++;
+                    roomsSolved++;
+                    roomCompleted = true;
                 }
             }
-        }
+            else if (input == "equation")
+            {
+                bool hasEquation = player.Inventory.Exists(i => i.Name.ToLower() == "equation");
+                if (!hasEquation)
+                {
+                    Console.WriteLine("\nYou don't have the equation yet.");
+                }
+                else
+                {
+                    Console.WriteLine("\nLook at this series: 53, 53, 40, 40, 27, 27, … What number should come next?");
+                    Console.Write("Your answer: ");
+                    string eqAnswer = Console.ReadLine().Trim();
 
-        public string PrintDescription(Room room)
-        {
-            return room.Description;
+                    if (eqAnswer == "14")
+                    {
+                        Console.WriteLine("\nCorrect! You solved the equation and find a key inside the glass box.");
+                        
+                        Item key = new Item("Key", "The key to your bedroom.");
+                        if (!player.Inventory.Exists(i => i.Name.ToLower() == "key"))
+                        {
+                            player.Add(key);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nThat's not quite right. Think about the sequence carefully.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nInput not recognized or puzzle already solved.");
+            }
         }
+        Console.WriteLine("Exiting room...");
+        Thread.Sleep(2000);
     }
 }
