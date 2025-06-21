13d42ada641c1120282726c65af430b8c68c5ff9
BoboParaskevov <bparaskevov007@gmail.com>
2025-06-21 14:58:24 +0300

    Update Item.cs

--- Escape Room/Escape Room/Item.cs
+++ Escape Room/Escape Room/Item.cs
@@ -6,23 +6,15 @@ using System.Threading.Tasks;
 
 namespace Escape_Room
 {
-       internal class Item
+          internal class Item
     {
-        private string name;
-        private string description;
-
-        public string Name { get => name; set => name = value; }
-        public string Description { get => description; set => description = value; }
+        public string Name { get; }
+        public string Description { get; }
 
         public Item(string name, string description)
         {
-            this.name = name;
-            this.description = description;
-        }
-
-        public override string ToString()
-        {
-            return name + " - " + description;
+            Name = name;
+            Description = description;
         }
     }
 }
